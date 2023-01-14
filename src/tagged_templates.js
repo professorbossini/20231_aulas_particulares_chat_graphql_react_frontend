10945a91f41420c2e745ecead009415bf50f748a
Rodrigo Bossini <professorbossini@gmail.com>
2023-01-13 22:04:38 -0300

    fim da secao 2.5 da apostila 03

---
+++ src/tagged_templates.js
@@ -0,0 +1,21 @@
+const nome = "Rodrigo"
+const idade = 20
+console.log (`Meu nome é ${nome} e tenho ${idade} anos`)
+
+const helloTaggedTemplates = () => console.log("Hello, Tagged Templates")
+// helloTaggedTemplates()
+
+helloTaggedTemplates``
+
+const podeDirigir = (strings, idade) => strings[0] + (idade >= 18 ? "" : "não ") + "pode dirigir"
+
+console.log(podeDirigir`Com 17 anos uma pessoa ${17}`)
+
+const gql = (strings) => {fewafweafewafwa}
+
+const proibidas = ["oi", "tchau"]
+
+suaFuncao ("oi, tudo bem? tchau")
+//***/, tudo bem? ***
+
+gql`fewafewafwa`
